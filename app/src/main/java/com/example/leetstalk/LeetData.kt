5623c883267c6ae07b0e4a5e387615126b26049d
Suryash <joblesscoder2468@gmail.com>
2023-06-13 19:26:48 +0530

    Gson Data parsing added and action bar removed

---
+++ app/src/main/java/com/example/leetstalk/LeetData.kt
@@ -0,0 +1,13 @@
+package com.example.leetstalk
+
+data class LeetData(
+	val status: Int? = null,
+	val easy: Int? = null,
+	val user: String? = null,
+	val medium: Int? = null,
+	val total: Int? = null,
+	val rating: Int? = null,
+	val hard: Int? = null,
+	val userProfileLink: String? = null
+)
+
--- app/src/main/java/com/example/leetstalk/MainActivity.kt
+++ app/src/main/java/com/example/leetstalk/MainActivity.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -25,22 +28,26 @@ class MainActivity : AppCompatActivity(){
             val medText= findViewById<TextView>(R.id.medCount)
             val hardText= findViewById<TextView>(R.id.hardCount)
 
+            val gson= GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create()
+
+
             val okHttpClient= OkHttpClient()
             val request= Request.Builder()
                 .url("http://192.168.1.4:8000/${leet_id}")
                 .build()
             GlobalScope.launch {
-                val response= withContext(Dispatchers.IO){okHttpClient.newCall(request).execute().body}
-                val JsonData= JSONObject(response?.string())
-                val easy= JsonData.getString("Easy")
-                val medium= JsonData.getString("Medium")
-                val hard= JsonData.getString("Hard")
+                val response= withContext(Dispatchers.IO){okHttpClient.newCall(request).execute().body?.string()}
+                val data= gson.fromJson<LeetData>(response, LeetData::class.java)
+                val easy= data.easy
+                val medium= data.medium
+                val hard= data.hard
 
                 Log.i("networking", "${response}")
                 runOnUiThread {
-                    easyText.text = easy
-                    medText.text = medium
-                    hardText.text = hard
+                    easyText.text = easy.toString()
+                    medText.text = medium.toString()
+                    hardText.text = hard.toString()
 
                 }
             }
