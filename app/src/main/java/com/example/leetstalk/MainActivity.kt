870d418a6903e047e27bb46aa49ee41987f4a6cf
Suryash <joblesscoder2468@gmail.com>
2023-06-13 10:34:55 +0530

    Json Data extraction working fine

--- app/src/main/java/com/example/leetstalk/MainActivity.kt
+++ app/src/main/java/com/example/leetstalk/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.leetstalk
 
 import androidx.appcompat.app.AppCompatActivity
-import android.text.method.ScrollingMovementMethod;
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -13,6 +12,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,17 +21,29 @@ class MainActivity : AppCompatActivity(){
         val btn= findViewById<Button>(R.id.submitButton)
         btn.setOnClickListener {
             val leet_id= findViewById<EditText>(R.id.git_id).text
-            val textView= findViewById<TextView>(R.id.apiResp)
+            val easyText= findViewById<TextView>(R.id.easyCount)
+            val medText= findViewById<TextView>(R.id.medCount)
+            val hardText= findViewById<TextView>(R.id.hardCount)
+
             val okHttpClient= OkHttpClient()
             val request= Request.Builder()
-                .url("http://192.168.1.2:8000/${leet_id}")
+                .url("http://192.168.1.4:8000/${leet_id}")
                 .build()
             GlobalScope.launch {
-                val response= withContext(Dispatchers.IO){okHttpClient.newCall(request).execute()}
-                Log.i("networking", "${response.body}")
-                textView.text= response.body?.string()
+                val response= withContext(Dispatchers.IO){okHttpClient.newCall(request).execute().body}
+                val JsonData= JSONObject(response?.string())
+                val easy= JsonData.getString("Easy")
+                val medium= JsonData.getString("Medium")
+                val hard= JsonData.getString("Hard")
+
+                Log.i("networking", "${response}")
+                runOnUiThread {
+                    easyText.text = easy
+                    medText.text = medium
+                    hardText.text = hard
+
+                }
             }
-            textView.movementMethod = ScrollingMovementMethod()
         }
 
         }
